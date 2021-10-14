5a5e0969932d97f918e0cf22022b72ea018c4de1
Bennett Smrdel <smrdelb@gmail.com>
2021-10-14 15:36:17 -0500

    finished required content

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+function listItems() {
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
+
+function empty() {
+  basket = [];
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding bannanas (expect true)', addItem('bannanas'));
+console.log(`Basket is now ${basket}`);
+
+empty();
+listItems();
