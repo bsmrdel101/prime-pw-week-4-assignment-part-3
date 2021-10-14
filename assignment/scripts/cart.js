04862a9e3144517f506e363f095326ceba89df69
Bennett Smrdel <smrdelb@gmail.com>
2021-10-14 16:09:09 -0500

    updated addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,17 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
 function addItem(item) {
-  basket.push(item);
-  return true;
+  let count = full();
+
+  if (count === false) {
+    basket.push(item);
+    return true;
+  } else {
+    return false;
+  }
 }
 function listItems() {
   for (let i = 0; i < basket.length; i++) {
@@ -17,10 +24,15 @@ function empty() {
   basket = [];
 }
 
+function full() {
+  let itemCount = (basket.length < maxItems) ? false : true;
+  return itemCount;
+}
+
 console.log(`Basket is ${basket}`);
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log('Adding bannanas (expect true)', addItem('bannanas'));
+console.log('Adding grapes (expect true)', addItem('grapes'));
 console.log(`Basket is now ${basket}`);
 
-empty();
 listItems();
